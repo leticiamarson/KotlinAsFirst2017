69aafdf750b1dd5573dfda2dab3e0432fddc9322
leticiamarson <31849731+leticiamarson@users.noreply.github.com>
2017-11-23 18:37:15 +0300

    Update IfElse.kt

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -146,19 +146,24 @@ fun triangleKind(a: Double, b: Double, c: Double): Int{
     var aa:Double = maxOf(a,b,c)
     var cc:Double = minOf(a,b,c)
     var bb:Double = 0.0
-    if(b==a &&b>cc){
-        bb=a
+    if(aa==a){
+        when(cc){
+            b -> bb=c
+            c -> bb=b
+        }
     }
-    else if(b==c && b<aa){
-        bb=cc
+    else if(aa==b){
+        when(cc){
+            a -> bb=c
+            c -> bb=a
+        }
     }
-    else if(a<aa && a>cc)
-        bb=a
-    else if(b<aa && b>cc)
-        bb=b
-    else
-        bb=c
-
+    else{
+        when(cc){
+            b -> bb=a
+            a -> bb=b
+        }
+    }
     when{
         aa<=0 || bb<=0 || cc<=0 -> return -1
         (a + b <= c) || (a + c <= b) || (b + c <= a) -> return -1
